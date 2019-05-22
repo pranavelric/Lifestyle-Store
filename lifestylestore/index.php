c1a1734937cfcc6e7fd23d65b7aa1f31fca23c9f
pranav <pranavchoudhary500@gmail.com>
2019-05-22 13:32:23 +0530

    slight changes

--- lifestylestore/index.php
+++ lifestylestore/index.php
@@ -35,28 +35,27 @@ session_start();
            </div>
            <div class="container">
                <div class="row">
-                   <div class="col-xs-4">
-                       <div  class="thumbnail">
-                           <a href="products.php">
-                                <img src="img/camera.jpg" alt="Camera">
+                 <div class="col-xs-4">
+                     <div  class="thumbnail">
+               <a href="products.php">
+                          <img src="img/camera.jpg" alt="Camera">
                            </a>
-                           <center>
-                                <div class="caption">
-                                        <p id="autoResize">Cameras</p>
-                                        <p>Choose among the best available in the world.</p>
-                                </div>
-                           </center>
-                       </div>
-                   </div>
-                   <div class="col-xs-4">
-                       <div class="thumbnail">
-                           <a href="products.php">
-                               <img src="img/watch.jpg" alt="Watch">
+                           <center>            <div class= "caption">
+         <p id="autoResize">Cameras</p>
+           <p>Choose among the best available in the world.</p>
+                  </div>
+                 </center>
+                 </div>
+                </div>
+         <div class="col-xs-4">
+           <div class="thumbnail">
+           <a href="products.php">
+                <img src="img/watch.jpg" alt="Watch">
                            </a>
-                           <center>
-                                <div class="caption">
-                                    <p id="autoResize">Watches</p>
-                                    <p>Original watches from the best brands.</p>
+                    <center>
+                <div class="caption">
+                   <p id="autoResize">Watches</p>
+                       <p>Original watches from the best brands.</p>
                                 </div>
                            </center>
                        </div>
@@ -68,8 +67,8 @@ session_start();
                            </a>
                            <center>
                                <div class="caption">
-                                   <p id="autoResize">Shirts</p>
-                                   <p>Our exquisite collection of shirts.</p>
+                   <p id="autoResize">Shirts</p>
+                  <p>Our exquisite collection of shirts.</p>
                                </div>
                            </center>
                        </div>
@@ -81,7 +80,7 @@ session_start();
                <div class="container">
                <center>
                    <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
-                   <p>This website is developed by Sajal Agrawal</p>
+                   
                </center>
                </div>
            </footer>
--- lifestylestore/success.php
+++ lifestylestore/success.php
@@ -48,7 +48,7 @@
                <div class="container">
                <center>
                    <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
-                   <p>This website is developed by Sajal Agrawal</p>
+                   
                </center>
                </div>
            </footer>
